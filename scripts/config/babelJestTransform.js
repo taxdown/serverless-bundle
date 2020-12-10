f93852f3c03cf2115010cdb3d7d775a7dd9f9f22
Joaquin Fernandez <joaquin@asperasoft.com>
2020-12-10 13:37:16 +0100

    Add babel plugin rewire

--- scripts/config/babelJestTransform.js
+++ scripts/config/babelJestTransform.js
@@ -4,7 +4,7 @@ const babelJest = require("babel-jest");
 
 module.exports = babelJest.createTransformer({
   presets: ["@babel/preset-env"],
-  plugins: ["@babel/plugin-proposal-class-properties"],
+  plugins: ["@babel/plugin-proposal-class-properties", "babel-plugin-rewire"],
   babelrc: false,
   configFile: false
 });
